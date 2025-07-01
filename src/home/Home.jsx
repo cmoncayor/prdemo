6a13dc7ecf4c1715ca4a78a0f16ef07147c29af2
carlos moncayo <cmoncayo@galgomedia.com>
2025-07-01 08:54:15 -0500

    youtube button

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -13,6 +13,14 @@ export const Home = () => (
       >
         Visit quave.dev
       </a>
+      <a
+        target="_blank"
+        href="https://www.youtube.com"
+        rel="noreferrer"
+        className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white hover:bg-indigo-700"
+      >
+        Visit YouTube
+      </a>
     </div>
     <div className="flex justify-center">
       <img className="w-1/2 md:w-1/3" src="./logo-with-shadow.png" alt="Vite Logo"/>
